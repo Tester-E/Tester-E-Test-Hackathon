3d00de55800718ef1c7781dfb1486dc7c62283ec
Team Tester-E <43983855+Tester-E@users.noreply.github.com>
2018-10-09 16:33:33 +0300

    Completed Payment

--- src/test/java/Definiton/MyStepDefs.java
+++ src/test/java/Definiton/MyStepDefs.java
@@ -4,13 +4,15 @@ import com.google.common.util.concurrent.Uninterruptibles;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.When;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import gherkin.lexer.Th;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class MyStepDefs {
@@ -37,10 +39,24 @@ public class MyStepDefs {
 
     }
 
+    @And("^I sign in \"([^\"]*)\" \"([^\"]*)\"$")
+    public void iSignIn(String email, String password) throws Throwable {
+        driver.findElement(By.cssSelector("#sign-in-btn")).click();
+        driver.findElement(By.cssSelector("#signin-email-input")).click();
+        driver.findElement(By.cssSelector("#signin-email-input")).sendKeys(email);
+        driver.findElement(By.cssSelector("#signin-password-input")).click();
+        driver.findElement(By.cssSelector("#signin-password-input")).sendKeys(password);
+        driver.findElement(By.cssSelector("#sign-in-submit-button")).click();
+        Thread.sleep(5000);
+    }
+
     @And("^I enter city \"([^\"]*)\"$")
     public void iEnterCity(String city) {
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.findElement(By.cssSelector(".SearchBoxTextEditor")).click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.findElement(By.cssSelector(".SearchBoxTextEditor")).sendKeys(city);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         Uninterruptibles.sleepUninterruptibly(1,TimeUnit.SECONDS);
         driver.findElement(By.cssSelector(".SearchBoxTextEditor")).sendKeys(Keys.ENTER);
     }
@@ -67,33 +83,169 @@ public class MyStepDefs {
         driver.findElement(By.cssSelector(".DropdownInput__box")).click();
         Thread.sleep(200);
 
-        Select dropdown = new Select(driver.findElement(By.cssSelector(".DropdownInput__box")));
-        dropdown.selectByIndex(3);
+        Select dropdown1 = new Select(driver.findElement(By.cssSelector(".DropdownInput__box")));
+        dropdown1.selectByIndex(3);
 
         }
 
     @And("^I click search$")
     public void iClickSearch() throws Throwable {
         driver.findElement(By.cssSelector("button.btn")).click();
+        Thread.sleep(5000);
     }
 
     @And("^I change currency$")
     public void iChangeCurrency() throws Throwable {
+        driver.findElement(By.cssSelector(".currency-trigger__text")).click();
+        driver.findElement(By.cssSelector("li.header-menu__item:nth-child(46) > a:nth-child(1)")).click();
+        Thread.sleep(5000);
     }
 
     @And("^I set price to max \"([^\"]*)\"$")
-    public void iSetPriceToMax(String arg0) throws Throwable {
+    public void iSetPriceToMax(String maxprice) throws Throwable {
+        driver.findElement(By.cssSelector("#price_box_0")).click();
+        driver.findElement(By.cssSelector("#price_box_0")).clear();
+        driver.findElement(By.cssSelector("#price_box_0")).sendKeys("0");
+        driver.findElement(By.cssSelector("#price_box_0")).sendKeys(Keys.TAB);
+        driver.findElement(By.cssSelector("#price_box_1")).sendKeys(maxprice);
+        driver.findElement(By.cssSelector("#price_box_1")).sendKeys(Keys.ENTER);
+        Thread.sleep(3000);
     }
 
     @And("^I select rating$")
     public void iSelectRating() throws Throwable {
+        driver.findElement(By.cssSelector(".ficon-star-5")).click();
+        Thread.sleep(200);
+        driver.findElement(By.cssSelector(".ficon-star-4")).click();
+        Thread.sleep(200);
+        driver.findElement(By.cssSelector(".ficon-star-3")).click();
+        Thread.sleep(200);
+    }
+
+    @And("^I select location$")
+    public void iSelectLocation() throws Throwable {
+        driver.findElement(By.cssSelector("#locationScoreFilterList > div:nth-child(2) > ul:nth-child(1) > li:nth-child(2) > span:nth-child(1)")).click();
+        Thread.sleep(200);
     }
 
     @And("^I choose \"([^\"]*)\"$")
     public void iChoose(String arg0) throws Throwable {
+
+        if (arg0.equals("Family/child friendly")){
+            driver.findElement(By.cssSelector("#facilitiesFilterList > div.filter-items > div > a")).click();
+            Thread.sleep(200);
+            driver.findElement(By.cssSelector("#facilitiesFilterList > div.filter-items > ul > li:nth-child(7) > span")).click();
+            Thread.sleep(500);
+            }
+
+        else if (arg0.equals("Free cancellation")){
+            driver.findElement(By.cssSelector("#paymentoptionFilterList > div:nth-child(2) > ul:nth-child(1) > li:nth-child(2) > span:nth-child(1) > span:nth-child(2) > span:nth-child(1)")).click();
+            Thread.sleep(1000);
+
+        }else if (arg0.equals("Non-smoking")){
+            driver.findElement(By.cssSelector("li.filter-item-react:nth-child(8) > span:nth-child(1)")).click();
+            Thread.sleep(1000);
+        }
+
+        else {
+        }
+
     }
 
-    @And("^I click on neihborhoods$")
-    public void iClickOnNeihborhoods() throws Throwable {
+
+    @And("^I sort the properties by \"([^\"]*)\"$")
+    public void iSortThePropertiesBy(String sortcriteria) throws Throwable {
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        if (sortcriteria.equals("Lowest price first")){
+
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.findElement(By.cssSelector(".sort-bar > a:nth-child(4)")).click();
+            Thread.sleep(3000);
+        }
+
+        else if (sortcriteria.equals("GUEST RATING")){
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.findElement(By.cssSelector(".dropdown-wrapper")).click();
+            Thread.sleep(3000);
+
+        }else if (sortcriteria.equals("SECRET DEALS")){
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.findElement(By.cssSelector(".selected")).click();
+            Thread.sleep(3000);
+        }
+
+        else {
+        }
+
     }
+
+    @And("^I select the property$")
+    public void iSelectTheProperty() throws Throwable {
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.cssSelector("#hotel-461536-container > div:nth-child(1) > div:nth-child(1)")).click();
+        Thread.sleep(5000);
+
+        Set <String> st= driver.getWindowHandles();
+        Iterator<String> it = st.iterator();
+        String parent =  it.next();
+        String child =it.next();
+        //swtich to parent
+        driver.switchTo().window(parent);
+        System.out.println("Returned to parent");
+        // switch to child
+        driver.switchTo().window(child);
+
+        driver.findElement(By.cssSelector("div.RoomGridFilter-filter:nth-child(1)")).click();
+        Thread.sleep(500);
+        driver.findElement(By.cssSelector("div.RoomGridFilter-filter:nth-child(2)")).click();
+        Thread.sleep(500);
+
+        driver.findElement(By.cssSelector(".ficon-mmb-inbox")).click();
+        Thread.sleep(1000);
+
+
+        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        jse.executeScript("window.scrollBy(0,500)", "");
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.cssSelector("#MasterRoom-3088136 > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(5) > div:nth-child(1) > div:nth-child(1) > button:nth-child(1)")).click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.cssSelector("div.PaymentOptionsModal-detail:nth-child(3) > button:nth-child(4)")).click();
+        Thread.sleep(2000);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.cssSelector("div.form-group:nth-child(4) > div:nth-child(1) > label:nth-child(2)")).click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        Thread.sleep(500);
+        driver.findElement(By.cssSelector(".btn")).click();
+    }
+
+
+
+    @And("^I enter card info \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\"$")
+    public void iEnterCardInfo(String cardtype, String cardno, String holdername, String date, String cvccvv) throws Throwable {
+
+        if (cardtype.equals("My saved cards")){
+            Select dropdown2 = new Select(driver.findElement(By.cssSelector("#paymentMethod")));
+            dropdown2.selectByIndex(1);
+        }
+
+        else if (cardtype.equals("Visa / MasterCard / JCB / Amex")){
+            Select dropdown2 = new Select(driver.findElement(By.cssSelector("#paymentMethod")));
+                    dropdown2.selectByIndex(2);
+        }
+
+        else {
+        }
+
+        driver.findElement(By.cssSelector("#cardNumber")).click();
+        driver.findElement(By.cssSelector("#cardNumber")).sendKeys(cardno);
+        driver.findElement(By.cssSelector("#cardHolderName")).click();
+        driver.findElement(By.cssSelector("#expiryDate")).sendKeys(holdername);
+        driver.findElement(By.cssSelector("#cardNumber")).click();
+        driver.findElement(By.cssSelector("#expiryDate")).sendKeys(date);
+        driver.findElement(By.cssSelector("#cvvCode")).click();
+        driver.findElement(By.cssSelector("#cvvCode")).sendKeys(cvccvv);
+
 }
--- src/test/java/Feature/SearchAndBook.feature
+++ src/test/java/Feature/SearchAndBook.feature
@@ -2,6 +2,7 @@ Feature: Search the properties and book your favorite
 
   Scenario: Search and Book
     When I go to "https://www.agoda.com/"
+    And I sign in "burak.boluk@hotmail.com" "12345678"
     And I enter city "Paris"
     And I choose dates "" ""
     And I choose guest and room
@@ -10,7 +11,10 @@ Feature: Search the properties and book your favorite
     And I change currency
     And I set price to max "166"
     And I select rating
-    And I choose "Family Friendly"
-    And I choose "Free Cancellation"
-    And I choose "Non-Smoking"
-    And I click on neihborhoods
+    And I select location
+    And I choose "Family/child friendly"
+    And I choose "Free cancellation"
+    And I choose "Non-smoking"
+    And I sort the properties by "Lowest price first"
+    And I select the property
+    And I enter card info "Visa / MasterCard / JCB / Amex" "5578292851390447" "Hazal Gunduz" "1022" "006"
